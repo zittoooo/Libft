ad956ed51934a30ef969ea8ff0742d416d525dc3
jiholee <stellar7484@naver.com>
2020-09-28 22:15:29 +0900

    ft_atoi.c

---
+++ ft_atoi.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+int     ft_atoi(const char *str)
+{
+    int     sign;
+    int     res;
+    int     i;
+
+    res = 0;
+    sign = 1;
+    i = 0;
+
+    while(*str  && ( *str == ' ' || *str == '\n' || *str == '\t' ||
+        *str == '\v' || *str == '\f' || *str == '\r') )
+        ++str;
+    if(*str == '-')
+        sign = -1;
+
+    if(*str == '-' || *str == '+')
+        ++str;
+    while(*str && *str >= '0' && *str <= '9')
+    {
+        res = res *10 + (*str - '0');
+        ++str;
+    }
+    return (res * sign);
+}
