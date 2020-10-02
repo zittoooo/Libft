b679468f7711dfaf209e006320d3f655802eaa0f
jiholee <stellar7484@naver.com>
2020-10-02 21:31:21 +0900

    add

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -11,12 +11,12 @@ char *ft_strchr(const char *s, int c)
     while (s[i])
     {
         if (s[i] == (char)c)
-            return ((char *)s + i);
+            return ((char *)(s + i));
         i++;
     }
 
     if (s[i] == (char)c)
-        return ((char *)s + i);
+        return ((char *)(s + i));
 
     return (NULL);
 }
