3469b404b8423438c3270123dcee0c2c422bd1a2
jiholee <stellar7484@naver.com>
2020-10-03 00:18:52 +0900

    20201003

--- libft/libft.h
+++ libft/libft.h
@@ -4,6 +4,12 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+typedef	struct	s_list
+{
+	void			*content;
+	struct s_list	*next;
+}				t_list;
+
 int				ft_atoi(const char *str);
 void			ft_bzero(void *s, size_t n);
 void			*ft_calloc(size_t count, size_t size);
@@ -12,8 +18,14 @@ int				ft_isalpha(int c);
 int				ft_isascii(int c);
 int				ft_isdigit(int c);
 int				ft_isprint(int c);
-
 char			*ft_itoa(int n);
+
+void			ft_lstadd_back(t_list **lst, t_list *new);
+void			ft_lstadd_front(t_list **lst, t_list *new);
+t_list			*ft_lstlast(t_list *lst);
+t_list			*ft_lstnew(void *content);
+int				ft_lstsize(t_list *lst);
+
 void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
@@ -35,17 +47,11 @@ size_t			ft_strlcpy(char *dst, const char *src, size_t size);
 size_t			ft_strlen(const char *s);
 
 char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
-
 char			*ft_strnstr(const char *s1, const char *s2, size_t n);
-
 char			*ft_strrchr(const char *s, int c);
-
 char			*ft_strtrim(char const *s1, char const *set);
-
 char			*ft_substr(char const *s, unsigned int start, size_t len);
-
 
 int				ft_tolower(int c);
 int				ft_toupper(int c);
