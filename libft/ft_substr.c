b5f5e86ec1d0df1df6359a5680662ba8bb496f77
jiholee <stellar7484@naver.com>
2020-10-05 13:31:47 +0900

    add

---
+++ libft/ft_substr.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+
+char    *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    size_t  i;
+    size_t  j;
+    char    *str;
+
+    str = (char *)malloc(sizeof(char) * (len + 1));
+    if(!str)
+        return (NULL);
+
+    i = 0;
+    j = 0;
+    while(s[i])
+    {
+        if(i >= start && j < len)
+        {
+            str[j] = s[i];
+            j++;
+        }
+        i++;
+    }
+    str[j] = '\0';
+    return (str);
+}
