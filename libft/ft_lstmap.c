8388115c1a356f54882e0f5876caa16a61a6c4b3
jiholee <stellar7484@naver.com>
2020-10-06 13:24:28 +0900

    20201006

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -2,5 +2,21 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*new_lst;
+	t_list	*new_elem;
 
+	if(!f || !del)
+		return (NULL);
+		new_lst = NULL;
+	while(lst)
+	{
+		if(!(new_elem = ft_lstnew(f(lst->content))))
+		{
+			ft_lstclear(&new_lst, del);
+			return(NULL);
+		}
+		ft_lstadd_back(&new_lst, new_elem);
+		lst = lst->next;
+	}
+	return (new_lst);
 }
